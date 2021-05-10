6481a2c25b0f211decedf61c84350dc80b3d205b
Meandres <ilya.meignan-masson@mailoo.org>
2021-05-11 00:31:21 +0200

    haha

---
+++ src/Controller/atomController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class atomController extends AbstractController
+{
+  #[Route('/', name: 'accueil')]
+  public function normal(): Response
+  {
+    return $this->render('accueil.html.twig');
+  }
+}
